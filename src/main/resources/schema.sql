72ebc6ed20f1add3cf68f639b3f146fa24ea234b
myoungso.kang <myoungso.kang@rakuten.com>
2019-10-21 13:59:57 +0900

    update field type Long to String

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,11 +3,11 @@ drop table if exists user;
 drop table if exists user_roles;
 
 create table ride_request (
-        id bigint not null,
-        address varchar(255) not null,
-        driver_id bigint,
-        passenger_id bigint not null,
+       id bigint not null,
+        address varchar(200) not null,
+        passenger_name varchar(255) not null,
         requested_at datetime not null,
+        driver_name varchar(255),
         responded_at datetime,
         version bigint,
         primary key (id)
